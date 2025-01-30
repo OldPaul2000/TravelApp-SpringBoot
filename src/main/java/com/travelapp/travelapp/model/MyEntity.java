39cdc004d02588c73173882fdf201908772503d1
Batrin Paul <paulbatrin@yahoo.com>
2025-01-30 18:32:06 +0200

    setting proper id data type

--- src/main/java/com/travelapp/travelapp/model/MyEntity.java
+++
@@ -1,8 +0,0 @@
-package com.travelapp.travelapp.model;
-
-/**
- * FOR REMOVING. STILL IT HAS SOME USAGES IN repository/testing package classes.
- * It must be removed only after removing usages
- */
-public interface MyEntity {
-}
